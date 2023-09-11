d388d469291ff6c534ff58de570b65f0ee362be3
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-09-11 18:58:08 +0200

    chore: adds colormind api access

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,29 @@
     components: {
       PaletteList
     },
+    data() {
+      return {
+        url: "http://colormind.io/api/",
+        data: {
+          model : "default"
+        },
+        palette: []
+      }
+    },
     methods: {
-      generatePalette: () => {
-        console.log("generatePalette called!");
+      generatePalette: function () {
+        var http = new XMLHttpRequest()
+
+        http.onreadystatechange = function() {
+          if(http.readyState == 4 && http.status == 200) {
+            var palette = JSON.parse(http.responseText).result
+            console.log(palette)
+            // TODO: Use palette to show PaletteItems inside of PaletteList
+          }
+        }
+
+        http.open("POST", this.url, true)
+        http.send(JSON.stringify(this.data))
       }
     }
   }
